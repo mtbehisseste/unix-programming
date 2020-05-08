aab1ee6f3a91a57210e805732b71608f3eec4340
mtbehisseste <monkey860315@gmail.com>
2020-05-09 01:00:20 +0800

    Implement basic features

--- hw2/sandbox.c
+++ hw2/sandbox.c
@@ -15,98 +15,71 @@
 
 // paramName is the parameters with only names, paramDecl is the parameters with
 // types and names
-#define injFunc(func, retType, defaultRetVal, paramFmt, paramName,           \
-                paramDecl...)                                                \
-    static retType (*old_##func)(paramDecl) = NULL;                          \
-    retType func(paramDecl)                                                  \
-    {                                                                        \
-        if (!old_##func) {                                                   \
-            void *handle =                                                   \
-                dlopen("/lib/x86_64-linux-gnu/libc.so.6", RTLD_LAZY);        \
-            if (handle)                                                      \
-                old_##func = dlsym(handle, #func);                           \
-            else                                                             \
-                fprintf(stderr, "[Sandbox] %s: Opening C library error",     \
-                        #func);                                              \
-        }                                                                    \
-        if (old_##func) {                                                    \
-            fprintf(stderr, "%s ", #func);                                   \
-            char *abs = isSubDir(paramFmt, paramName);                       \
-            if (abs) {                                                       \
-                free(abs);                                                   \
-                return old_##func paramName;                                 \
-            }                                                                \
-            else {                                                           \
-                fprintf(stderr, "[Sandbox] %s: access to %s is not allowed", \
-                        #func, abs);                                         \
-                free(abs);                                                   \
-                return defaultRetVal;                                        \
-            }                                                                \
-        }                                                                    \
+#define INJ_FUNC(func, retType, defaultRetVal, paramName,  paramDecl...)        \
+    retType func(paramDecl)                                                     \
+    {                                                                           \
+        int isSub = isSubDir(#func, path);                                      \
+        if (!isSub)                                                             \
+            return defaultRetVal;                                               \
+        static retType (*old_##func)(paramDecl) = NULL;                         \
+        void *handle = dlopen("/lib/x86_64-linux-gnu/libc-2.27.so", RTLD_LAZY); \
+        if (handle) {                                                           \
+            old_##func = dlsym(handle, #func);                                  \
+        } else {                                                                \
+            fprintf(stderr, "[Sandbox] %s: Opening C library error\n",  #func); \
+        }                                                                       \
+        return old_##func paramName;                                            \
+    }                                                                          
+
+#define RESTRICT_FUNC(func, retType, defaultRetVal, paramName, param...) \
+    retType func(param)                                                  \
+    {                                                                    \
+        fprintf(stderr, "[Sandbox] %s: not allowed\n", #func);           \
+        return defaultRetVal;                                            \
     }
 
-#define restrictFunc(func, retType, defaultRetVal, paramName, param...) \
-    retType func(param)                                                 \
-    {                                                                   \
-        fprintf(stderr, "[Sandbox] %s: not allowed\n", #func);          \
-        return defaultRetVal;                                           \
-    }
-
-// Returns the absolute path of the subdirectory if it is a subdirectory of 
-// the base directory, return NULL if not
-char *isSubDir(char *fmt, ...)
+int isSubDir(char* func, const char *path) 
 {
-    va_list ap;
-    const char *s;
-
     char *basedir = getenv("BASEDIR");
     char *absPath = NULL;
 
-    va_start(ap, fmt);
-    while (*fmt) {
-        switch (*fmt++) {
-        // since we only care about the pathname, so string is enough
-        case 's':
-            s = va_arg(ap, char *);
-            absPath = realpath(s, absPath);
-            printf("s abs: %s %s %s\n", s, absPath, basedir);
-            if (absPath && strncmp(basedir, absPath, strlen(basedir)) <= 0)
-                return absPath;
-            break;
-        // we don't care for other types of parameters
-        default:
-            break;
-        }
+    absPath = realpath(path, absPath);
+    // path not found or not a subdirectory
+    if (!absPath || strncmp(basedir, absPath, strlen(basedir)) > 0) {
+        fprintf(stderr, "[Sandbox] %s: access to %s is not allowed\n", func, path);
+        free(absPath);
+        return 0;
     }
-    va_end(ap);
-
-    return NULL;
+    return 404;
 }
 
-injFunc(chdir, int, -1, "s", (path), const char *path);
-injFunc(chmod, int, -1, "sd", (pathname, mode), const char *pathname, mode_t mode);
-injFunc(chown, int, -1, "sdd", (pathname, owner, group), const char *pathname, uid_t owner, gid_t group);
-injFunc(creat, int, -1, "sd", (pathname, mode), const char *pathname, mode_t mode);
-injFunc(fopen, FILE *, NULL, "sd", (pathname, mode), const char *pathname, const char *mode);
-injFunc(link, int, -1, "ss", (oldpath, newpath), const char *oldpath, const char *newpath);
-injFunc(mkdir, int, -1, "sd", (pathname, mode), const char *pathname, mode_t mode);
-injFunc(open, int, -1, "sd", (pathname, flags), const char *pathname, int flags);
-injFunc(openat, int, -1, "dsd", (dirfd, pathname, flags), int dirfd, const char *pathname, int flags);
-injFunc(opendir, DIR *, NULL, "s", (name), const char *name);
-injFunc(readlink, ssize_t, -1, "sdd", (pathname, buf, bufsize), const char *pathname, char *buf, size_t bufsize);
-injFunc(remove, int, -1, "s", (pathname), const char *pathname);
-injFunc(rename, int, -1, "s s", (oldpath, newpath), const char *oldpath, const char *newpath);
-injFunc(rmdir, int, -1, "s", (pathname), const char *pathname);
-injFunc(stat, int, -1, "sd", (pathname, statbuf), const char *pathname, struct stat *statbuf);
-injFunc(symlink, int, -1, "ss", (target, linkpath), const char *target, const char *linkpath);
-injFunc(unlink, int, -1, "s", (pathname), const char *pathname);
+INJ_FUNC(chdir, int, -1, (path), const char *path);
+INJ_FUNC(chmod, int, -1, (path, mode), const char *path, mode_t mode);
+INJ_FUNC(chown, int, -1, (path, owner, group), const char *path, uid_t owner, gid_t group);
+INJ_FUNC(creat, int, -1, (path, mode), const char *path, mode_t mode);
+INJ_FUNC(fopen, FILE *, NULL, (path, mode), const char *path, const char *mode);
+INJ_FUNC(mkdir, int, -1, (path, mode), const char *path, mode_t mode);
+INJ_FUNC(open, int, -1, (path, flags), const char *path, int flags);
+INJ_FUNC(openat, int, -1, (dirfd, path, flags), int dirfd, const char *path, int flags);
+INJ_FUNC(opendir, DIR *, NULL, (path), const char *path);
+INJ_FUNC(readlink, ssize_t, -1, (path, buf, bufsize), const char *path, char *buf, size_t bufsize);
+INJ_FUNC(remove, int, -1, (path), const char *path);
+INJ_FUNC(rmdir, int, -1, (path), const char *path);
+INJ_FUNC(stat, int, -1, (path, statbuf), const char *path, struct stat *statbuf);
+INJ_FUNC(symlink, int, -1, (path, linkpath), const char *path, const char *linkpath);
+INJ_FUNC(unlink, int, -1, (path), const char *path);
 
-restrictFunc(execl, int, -1, (path, arg), const char *path, const char *arg, ...);
-restrictFunc(execle, int, -1, (path, arg), const char *path, const char *arg, ...);
-restrictFunc(execlp, int, -1, (path, arg), const char *path, const char *arg, ...);
-restrictFunc(execv, int, -1, (path, argv), const char *path, char *const argv[]); 
-restrictFunc(execvp, int, -1, (path, argv), const char *path, char *const argv[]);
-restrictFunc(execve, int, -1, (path, argv, envp), const char *filename, char *const argv[], char *const envp[]);
-restrictFunc(system, int, -1, command, const char *command);
+// TODO deals with the two paths functions
+/* INJ_FUNC(rename, int, -1, "s s", (oldpath, newpath), const char *oldpath, const char *newpath); */
+/* INJ_FUNC(link, int, -1, "ss", (oldpath, newpath), const char *oldpath, const char *newpath); */
+
+// TODO make sure the symbols in libc of all functions 
+RESTRICT_FUNC(execl, int, -1, (path, arg), const char *path, const char *arg, ...);
+RESTRICT_FUNC(execle, int, -1, (path, arg), const char *path, const char *arg, ...);
+RESTRICT_FUNC(execlp, int, -1, (path, arg), const char *path, const char *arg, ...);
+RESTRICT_FUNC(execv, int, -1, (path, argv), const char *path, char *const argv[]); 
+RESTRICT_FUNC(execvp, int, -1, (path, argv), const char *path, char *const argv[]);
+RESTRICT_FUNC(execve, int, -1, (path, argv, envp), const char *path, char *const argv[], char *const envp[]);
+RESTRICT_FUNC(system, int, -1, command, const char *command);
 
 // TODO handle error message
